0c1b9c0f7b6a3c68b009068b446054e4aff2f1b5
Abdullah-alnoumas <70814254+Abdullah-alnoumas@users.noreply.github.com>
2022-03-05 13:26:34 +0300

    hi

---
+++ ios-cw-4/MyPlayground uno.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var name = "عبدالله معتصم النومس"
+
+var hobby = "كرة القدم"
+
+var favoriteNumber = 7
+
+print( " السلام عليكم معاكم \(name) و رقمي المفضل هو \(favoriteNumber) و احب \(hobby) \n")
+
+ // \n means new line
+
+name = " معتصم ربيع النومس"
+
+favoriteNumber = 17
+
+hobby = " ركوب الخيل"
+
+print( " السلام عليكم معاكم \(name) و رقمي المفضل هو \(favoriteNumber) و احب \(hobby)")
